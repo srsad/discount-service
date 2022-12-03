537907e09518ef4fda7a8be25ff79907359802f1
Artak Tsakanyan <a.tsakanyan@assistagro.com>
2022-12-03 19:24:49 +0300

    добавил парсер для пятерочки

--- backend/app/cmd/main.go
+++ backend/app/cmd/main.go
@@ -1,13 +1,23 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	parser "github.com/srsad/discount-service/services/parsers"
+)
 
 func main() {
-    app := fiber.New()
+  // TODO разнести по разным сервисам - все парсеры запускать кроном
+  // для парсинга установить в true
+	parseProduct := false
+	if (parseProduct) {
+    parser.Parse5Ka()
+	}
 
-    app.Get("/", func(c *fiber.Ctx) error {
-        return c.SendString("Hello world!")
-    })
+	app := fiber.New()
 
-    app.Listen(":3000")
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello world!")
+	})
+
+	app.Listen(":3000")
 }
