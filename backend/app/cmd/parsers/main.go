ac759753b1878a9ea4720149d202b524f4a4204c
Artak Tsakanyan <a.tsakanyan@assistagro.com>
2023-01-01 15:04:00 +0300

    удалил лишние комменты

--- backend/app/cmd/parsers/main.go
+++ backend/app/cmd/parsers/main.go
@@ -4,9 +4,6 @@ import (
 	parser "github.com/srsad/discount-service/services/parsers"
 )
 
-// go run github.com/srsad/discount-service/cmd/parsers.go
-// go build -o ./tmp/parsers ./cmd/parsers.go
-
 func main() {
 	parser.Parse5Ka()
 }
